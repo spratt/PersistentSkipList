af72d2ce03e96e44396795985d6304ffb5a2f473
Simon Pratt <spratt@scs.carleton.ca>
2011-08-05 18:15:28 -0400

    mr.sh now tests timestamped array as well

--- mr.sh
+++ mr.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-make clean && make && rm psl.log && ./test_persistent_skiplist 2> psl.log
+make clean &&
+make &&
+rm psl.log &&
+./test_timestamped_array && ./test_persistent_skiplist 2> psl.log
